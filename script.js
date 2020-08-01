79e74282372b4fafc01f6420e980edd1b3dac25a
Clara Pasteau <clarapasteau@gmail.com>
2020-08-01 19:07:07 +0200

    eug start

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-var text = "Introduction à (hier: oui un jour)";
-var text2 = "La vieille fille sous le soleil mort";
-var text3 = "c'est les grandes vacances";
+var text = "Eugénie Zely";
 
 for(var i in text) { 
   if(text[i] === " ") {
@@ -9,18 +7,4 @@ for(var i in text) {
     $(".wavetext").append( $("<span>").text(text[i]) ); 
   }
 }
-for(var j in text2) { 
-    if(text2[j] === " ") {
-      $(".wavetext2").append( $("<span>").html("&nbsp;") ); 
-    } else {  
-      $(".wavetext2").append( $("<span>").text(text2[j]) ); 
-    }
-  }
-  for(var k in text3) { 
-    if(text3[k] === " ") {
-      $(".wavetext3").append( $("<span>").html("&nbsp;") ); 
-    } else {  
-      $(".wavetext3").append( $("<span>").text(text3[k]) ); 
-    }
-  }
 
