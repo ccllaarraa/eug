ca74d95d783e10b245576491f34139a18d46e450
Clara Pasteau <clarapasteau@gmail.com>
2020-08-26 17:32:31 +0200

    change menu

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+    
+$('body').append(' <div id="loading"><img id="loading-image" src="assets/goutte.png" alt="Loading..." /><img id="loading-image" src="assets/goutte.png" alt="Loading..." /><img id="loading-image" src="assets/goutte.png" alt="Loading..." /></div>');
+$(window).on('load', function(){
+  setTimeout(removeLoader); //wait for page load PLUS one seconds.
+});
+function removeLoader(){
+    $( "#loading" ).fadeOut(500, function() {
+      // fadeOut complete. Remove the loading div
+      $( "#loading" ).remove(); //makes page more lightweight 
+  });  
+}
+
+window.addEventListener("load", function load() {
+  document.getElementById("defaultOpen").click();
+  });
+  function openTab(evt, tabName) {
+      // Declare all variables
+      var i, tabcontent, tablinks;
+    
+      // Get all elements with class="tabcontent" and hide them
+      tabcontent = document.getElementsByClassName("tabcontent");
+      for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+      }
+    
+      // Get all elements with class="tablinks" and remove the class "active"
+      tablinks = document.getElementsByClassName("tablinks");
+      for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace(" active", "");
+      }
+    
+      // Show the current tab, and add an "active" class to the button that opened the tab
+      document.getElementById(tabName).style.display = "block";
+      evt.currentTarget.className += " active";
+    }
+  
+
 var text = "Eugenie Zely";
 var vacstitre1 = "C'est les vacances, 2019-2020";
 var vacsfilm="Un film";
